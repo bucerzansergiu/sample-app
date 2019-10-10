711c3a9e7c67dc77ea8b310cce724b649f26d519
Sergiu Bucerzan <sbucerzan@sessionm.com>
2019-10-10 16:28:29 +0300

    finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Bine boss"
+      redirect_to @user
       # Handle a successful save.
     else
       render 'new'
