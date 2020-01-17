f9953752255ef41afabd5191b23eae6a1ca7cec4
Sergiu Bucerzan <sbucerzan@sessionm.com>
2020-01-17 13:14:49 +0200

    added user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -67,4 +67,22 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.authenticated?(:remember, '')
   end
 
+  require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
+end
+
 end
